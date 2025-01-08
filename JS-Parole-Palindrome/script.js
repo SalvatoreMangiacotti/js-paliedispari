e638e3401b03757386a92691467fc77b8c8f495f
Salvatore <workstudioms@gmail.com>
2025-01-08 17:15:10 +0100

    Parole Palindrome, for loop to reverse a string, fix

--- JS-Parole-Palindrome/script.js
+++ JS-Parole-Palindrome/script.js
@@ -4,13 +4,21 @@
 
 const parolaInserita = prompt("Inserisci una parola : ");
 
-console.log(verificaParolaPalindroma(parolaInserita));
+console.log(verificaParolaPalindroma());
 
 // Creare una funzione per capire se la parola inserita è palindroma
 
-function verificaParolaPalindroma(stringa) {
+function verificaParolaPalindroma() {
 
-    const parolaInvertita = stringa.split("").reverse().join("");
+    // const parolaInvertita = stringa.split("").reverse().join("");
+
+    let parolaInvertita = "";
+
+    for (let i = parolaInserita.length - 1; i >= 0; i--) {
+
+        parolaInvertita = parolaInvertita + parolaInserita[i];
+
+    }
 
     // SE la parola inserita dall'utente è uguale alla parola inserita ma invertita
 
