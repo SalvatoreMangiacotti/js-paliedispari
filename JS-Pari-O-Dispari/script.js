1a299f7c8f24fa9e9bab1921e934cb09a5d6ec7d
Salvatore <workstudioms@gmail.com>
2025-01-08 14:59:47 +0100

    js-paliedispari, html and JS structure, done. Parole-Palindrone exercise, complese.

---
+++ JS-Pari-O-Dispari/script.js
@@ -0,0 +1,29 @@
+// Palidroma
+
+// Chiedere all’utente di inserire una parola
+
+const parolaInserita = prompt("Inserisci una parola");
+
+console.log(verificaParolaPalindroma(parolaInserita));
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+function verificaParolaPalindroma(parola) {
+
+    const parolaInvertita = parola.split("").reverse().join("");
+
+    // SE la parola inserita dall'utente è uguale alla parola inserita ma invertita
+
+    if (parolaInserita === parolaInvertita) {
+
+        // La parola inserita è palindroma (otto, oro, afa)
+
+        return "La parola inserita è palindroma";
+
+    } else {
+
+        return "La parola inserita NON è palindroma";
+
+    }
+
+}
